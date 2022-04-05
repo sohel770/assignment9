72e924937c1a8d7a73e749eac75e9def713149f3
sohel Ahamed <sohel770hossain@gmail.com>
2022-04-05 20:36:17 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './componetes/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes> 
+       <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
